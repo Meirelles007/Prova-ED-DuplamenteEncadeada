da6b55a46a52580a740609f3f1855b4167191c63
willi <willi@DESKTOP-689IO0P>
2021-12-02 23:05:38 -0300

    ASD

--- src/controller/ListaBuilder.java
+++ src/controller/ListaBuilder.java
@@ -1,28 +1,35 @@
 package controller;
 
 import java.util.LinkedList;
-import java.util.List;
+
 
 public class ListaBuilder {
-	public LinkedList<String> gerarlistas(String nome, LinkedList<String> lista) {
-		lista.add(nome);
-		return lista;
-	}
+    public LinkedList<String> gerarlistas(String nome, LinkedList<String> lista) {
+        lista.add(nome);
+        return lista;
+    }
 
-	public void concatenarlistas(LinkedList<String> lista1, LinkedList<String> lista2) {
-		LinkedList<String> listas_unidas = new LinkedList<String>();
-		int tamanholista1 = 0;
-		int tamanholista2 = lista2.size();
-		
-		if(tamanholista1>tamanholista2) {
-			while(true) {
-				listas_unidas.add(lista1.get(tamanholista1));
-				listas_unidas.add(lista2.get(tamanholista2));
-		
-				tamanholista1++;
-				tamanholista2--;
-			}
-		}
-		
-	}
+    public LinkedList<String> concatenarlistas(LinkedList<String> lista1, LinkedList<String> lista2) {
+        LinkedList<String> listas_unidas = new LinkedList<String>();
+        int tamanholista1 = 0;
+        int tamanholista2 = lista2.size() -1 ;
+
+        System.out.println(lista1.get(tamanholista1));
+        System.out.println(lista2.get(tamanholista2 -1 ));
+
+            while(true) {
+                if(tamanholista1>=lista1.size()) {
+                    break;
+                }
+                if(tamanholista2==0) {
+                    break;
+                }
+
+                listas_unidas.add(lista1.get(tamanholista1));
+                listas_unidas.add(lista2.get(tamanholista2));
+                tamanholista1++;
+                tamanholista2--;
+            }
+        return listas_unidas;
+    }
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,31 +1,33 @@
 package view;
 
 import java.util.LinkedList;
-import java.util.List;
-import javax.swing.JOptionPane;
 import controller.ListaBuilder;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		LinkedList<String> lista1 = new LinkedList<String>();
-		LinkedList<String> lista2 = new LinkedList<String>();
-		LinkedList<String> lista_decrescente = new LinkedList<String>();
+    public static void main(String[] args) {
+        LinkedList<String> lista1 = new LinkedList<String>();
+        LinkedList<String> lista2 = new LinkedList<String>();
+        LinkedList<String> unidas = new LinkedList<String>();
 
-		ListaBuilder builder = new ListaBuilder();
+        ListaBuilder builder = new ListaBuilder();
 
-		String[] nome = { "H", "U", "G", "O" };
-		String[] sobrenome = { "V", "I", "N", "I", "C", "I", "O", "S" };
+        String[] nome = { "W", "I", "L", "L" };
+        String[] sobrenome = { "M", "E", "I", "R", "E", "L", "L", "E","S" };
 
-		for (int i = 0; i < nome.length; i++) {
-			lista1 = builder.gerarlistas(nome[i], lista1);
-		}
+        for (int i = 0; i < nome.length; i++) {
+            lista1 = builder.gerarlistas(nome[i], lista1);
+        }
 
-		for (int i = 0; i < sobrenome.length; i++) {
-			lista2 = builder.gerarlistas(sobrenome[i], lista2);
-		}
+        for (int i = 0; i < sobrenome.length; i++) {
+            lista2 = builder.gerarlistas(sobrenome[i], lista2);
+        }
 
-		builder.concatenarlistas(lista1, lista2);
+        unidas = builder.concatenarlistas(lista1, lista2);
 
-	}
+        System.out.println("Lista Nome" + lista1.toString());
+        System.out.println("Lista Primeiro Sobrenome" + lista2.toString());
+        System.out.println("Concatenadas" + unidas.toString());
+
+    }
 }
